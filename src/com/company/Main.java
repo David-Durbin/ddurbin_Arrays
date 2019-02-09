1c5e34b87aeeda154ba16cfb4dd232d6fd6070e0
David Durbin <David-Durbin@users.noreply.github.com>
2019-02-08 18:18:54 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] items = {"napkins", "spoons", "small foam cups", "large foam cups", "sugar cones",
+                "small waffle cones", "large waffle cones", "ice cream scoops"};
+
+	    for(int i = 0; i < items.length; i++)
+        {
+            System.out.println(items[i]);
+        }
+    }
+}
